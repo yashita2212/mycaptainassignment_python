cb8bcfa5924f8beab72293193f8c50a4c797079a
yashita2212 <122201456+yashita2212@users.noreply.github.com>
2023-01-29 17:28:02 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Task 1 
+# Write a Python program which accepts the radius of a circle from the user and computes area.
+
+r = int(input("Enter radius of the circle: "))
+print("Area of the circle is: ", 3.14*r**2) 
+
+
+
+
+
+    
+    
+        
+    
+
+    
+    
+    
+    
+    
+    
+    
+
