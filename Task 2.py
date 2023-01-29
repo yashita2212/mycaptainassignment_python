8f61d5aaf364d37d124be915d7b577ad2b1622ef
yashita2212 <122201456+yashita2212@users.noreply.github.com>
2023-01-29 17:38:43 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 17:33:57 2023
+
+@author: Yashita
+"""
+# Task 2 
+# Write a Python program to accept a filename from the user and print the extension of that.
+
+x = input("Enter filename: ")
+y = x.split(".")
+print("File extension is: ", y[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
