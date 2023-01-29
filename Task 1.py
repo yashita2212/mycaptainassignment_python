f4901c3e5ee229316ce7849d8eb2c74eda7a55e4
yashita2212 <122201456+yashita2212@users.noreply.github.com>
2023-01-29 17:24:43 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 17:01:32 2023
+
+@author: Yashita
+"""
+# Task 1
+# 
+
+r = int(input("Enter area of the circle: "))
+print("Area of the circle is: ", 3.14*r**2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
